9f0ef4abcafe711ebff5988565ff10fa4a049c25
poncoe <arief.wardhana@itasoft.co.id>
2025-08-17 18:24:08 +0700

    configure routes in controller admin & auth

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -7,5 +7,8 @@ use Illuminate\Http\Request;
 
 class AdminController extends Controller
 {
-    //
+    public function adminDashboard(Request $request) {
+        $data = ['pageTitle' => 'Dashboard'];
+        return View('back.pages.auth.dashboard', $data);
+    }
 }
--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -7,5 +7,13 @@ use Illuminate\Http\Request;
 
 class AuthController extends Controller
 {
-    //
+    public function loginForm(Request $request) {
+        $data = ['pageTitle' => 'Login'];
+        return View('back.pages.auth.login', $data);
+    }
+
+    public function forgotForm(Request $request) {
+        $data = ['pageTitle' => 'Forgot Password'];
+        return View('back.pages.auth.forgot', $data);
+    }
 }
